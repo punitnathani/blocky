8d8164c3a8ff48a5e08295956cabec818d083ff8
Punit Nathani <corporate2punit@live.com>
2018-04-24 13:50:11 +0530

    First iteration of blockchain

---
+++ src/main.js
@@ -0,0 +1,42 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+  constructor(index, timestamp, data, previousHash = '') {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
+  
+  calculateHash() {
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];  // array of blocks
+    
+  }
+  
+  createGenesisBlock() {
+    return new Block(0, "01/01/2018", "Genesis block", "0");
+  }
+  
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+  
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+}
+
+let blockyCoin = new Blockchain();
+blockyCoin.addBlock(new Block(1, "10/01/2018", {amount: 4}));
+blockyCoin.addBlock(new Block(2, "20/01/2018", {amount: 10}));
+
+console.log(JSON.stringify(blockyCoin, null, 4));
